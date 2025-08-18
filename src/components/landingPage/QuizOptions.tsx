8789e3737ec31d18ddc5722f24dfef66b83db261
rodigb <rodix08@gmail.com>
2025-08-19 00:31:58 +0100

    small text added

--- src/components/landingPage/QuizOptions.tsx
+++ src/components/landingPage/QuizOptions.tsx
@@ -6,48 +6,58 @@ function QuizOptions() {
   const dispatch = useAppDispatch();
 
   return (
-    <List
-      component="div"
-      disablePadding
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: { xs: "center", sm: "flex-start" },
-        alignItems: "center",
-      }}
-    >
-      <ListItemButton
+    <>
+      <Typography
+        variant="h3"
+        align="center"
+        sx={{ color: "#ffffff", fontWeight: "bold" }}
+        gutterBottom
+      >
+        Choose an artist
+      </Typography>
+      <List
+        component="div"
+        disablePadding
         sx={{
-          height: "250px",
-          width: "250px",
           display: "flex",
-          justifyContent: "flex-end",
           flexDirection: "column",
+          justifyContent: { xs: "center", sm: "flex-start" },
           alignItems: "center",
-          backgroundImage: `url(https://frontend-interview.evidentinsights.com/album_covers/01)`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          position: "relative",
-        }}
-        id={"The Beatles"}
-        onClick={(e) => {
-          dispatch(setArtist(e.currentTarget.id));
         }}
       >
-        <Box
+        <ListItemButton
           sx={{
-            width: "100%",
-            textAlign: "center",
-            bgcolor: "#FF7129",
-            p: 1,
+            height: "250px",
+            width: "250px",
+            display: "flex",
+            justifyContent: "flex-end",
+            flexDirection: "column",
+            alignItems: "center",
+            backgroundImage: `url(https://frontend-interview.evidentinsights.com/album_covers/01)`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            position: "relative",
+          }}
+          id={"The Beatles"}
+          onClick={(e) => {
+            dispatch(setArtist(e.currentTarget.id));
           }}
         >
-          <Typography sx={{ color: "white", fontWeight: "bold" }}>
-            The Beatles
-          </Typography>
-        </Box>
-      </ListItemButton>
-    </List>
+          <Box
+            sx={{
+              width: "100%",
+              textAlign: "center",
+              bgcolor: "#FF7129",
+              p: 1,
+            }}
+          >
+            <Typography sx={{ color: "white", fontWeight: "bold" }}>
+              The Beatles
+            </Typography>
+          </Box>
+        </ListItemButton>
+      </List>
+    </>
   );
 }
 
